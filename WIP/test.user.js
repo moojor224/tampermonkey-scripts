b2d9c171e65e6b86e8fe05f0e2d93ece3b8caa25
moojor224 <moojor224@gmail.com>
2024-06-13 17:03:18 -0500

    add testing file

---
+++ WIP/test.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         test
+// @version      1
+// @description  description
+// @author       moojor224
+// @match        https://*/*
+// ==/UserScript== 
+
+(async function () {
+    let { } = await import("https: //cdn.jsdelivr.net/gh/moojor224/random-files@latest/js/synced/jstools.js").then(e => e);
+    
+})();
